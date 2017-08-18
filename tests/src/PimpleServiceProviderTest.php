fe27dc9a2e9d6770f8254b4fd4d237857a0d91e7
Carsten Witt <carsten.witt@gmail.com>
2017-08-18 09:37:33 +0200

    Completed test

---
+++ tests/src/PimpleServiceProviderTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace tests;
+
+use Germania\Base64Coder\Providers\PimpleServiceProvider;
+
+use Pimple\ServiceProviderInterface;
+use Pimple\Container;
+use Psr\Log\NullLogger;
+use Psr\Log\LoggerInterface;
+
+class PimpleServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    /**
+     * @dataProvider provideCtorArguments
+     */
+    public function testRegisteringServiceProvider( $separator, $logger)
+    {
+        $dic = new Container;
+
+        $sut = new PimpleServiceProvider;
+        $sut->register( $dic );
+
+        $this->assertInstanceOf( ServiceProviderInterface::class, $sut );
+
+        $this->assertTrue( is_callable( $dic['Cookie.Encryptor']) );
+        $this->assertTrue( is_callable( $dic['Cookie.Decryptor']) );
+
+    }
+
+    public function provideCtorArguments()
+    {
+        $logger = $this->prophesize( LoggerInterface::class );
+
+        return [
+            [ '::', new NullLogger ],
+            [ null, null ],
+            [ null, $logger->reveal() ]
+        ];
+    }
+}
